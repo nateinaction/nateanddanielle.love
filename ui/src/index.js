a380b1b02864d40c2f32d377e2b7de3665238e0c
Nate Gay <nate.gay@wpengine.com>
2017-08-07 00:36:09 -0500

    v0.5.0 - Added menu bar and custom theme. No actual menu items yet though...

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,23 +3,41 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
-import { MuiThemeProvider } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 //import { red, purple } from 'material-ui/colors';
 import store from './store'
-//import Menu from './containers/Menu'
+import Menu from './containers/Menu'
 import Posts from './containers/Posts'
 //import Test from './components/test'
 
 //import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
+import createPalette from 'material-ui/styles/palette';
+import blue from 'material-ui/colors/blue';
+import red from 'material-ui/colors/red';
+
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: red,
+    accent: {
+      ...blue,
+      A200: '#90CAF9',
+    },
+    error: red,
+  }),
+});
+
 //const primary = red[500]; // #F44336
 //const accent = purple['A200']; // #E040FB
 
 render(
   <Provider store={store}>
-    <MuiThemeProvider>
-      <Posts />
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <Menu />
+        <Posts />
+      </div>
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
